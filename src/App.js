1ebf629c8022a80bb0024423149740a28361919b
Safwan Ali <safwan4872@gmail.com>
2023-05-18 20:49:25 +0600

    lms clone

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainApp from "./components/MainApp";
+
+const App = () => {
+  return (
+    <div>
+      <MainApp />
+    </div>
+  );
+};
+
+export default App;
